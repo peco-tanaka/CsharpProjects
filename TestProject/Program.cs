c7617741c8412913c2d3b638c8782ca13a6847aa
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-18 18:57:17 +0900

    不正注文IDのロジックを削除し、名前の表示と在庫の合計を計算する機能を実装

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,20 +1,21 @@
-﻿
-/* string[] fraudulentOrderIDs = new string[3];
+﻿string[] names = ["Rowena", "Robin", "Bao"];
 
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789"; */
-// fraudulentOrderIDs[3] = "D000";
+foreach (string name in names)
+{
+  Console.WriteLine(name);
+}
 
+int[] inventory = [200, 450, 700, 175, 250];
 
-string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
+int sum = 0;
 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+int bin = 0;
 
-fraudulentOrderIDs[0] = "F000";
+foreach (int item in inventory)
+{
+  sum += item;
+  bin++;
+  Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
+}
 
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+Console.WriteLine($"We have {sum} items in inventory.");

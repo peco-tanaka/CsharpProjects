2eb3df3f1208318e3cd6e8ac9636662483a966bf
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-18 18:34:13 +0900

    不正注文IDの配列を追加し、各要素を表示する機能を実装

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,5 +1,20 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
+﻿
+/* string[] fraudulentOrderIDs = new string[3];
 
-Console.WriteLine(largerValue);
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789"; */
+// fraudulentOrderIDs[3] = "D000";
+
+
+string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");

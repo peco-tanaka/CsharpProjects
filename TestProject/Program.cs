ba974ec470ccf7ada79d9062eb1071cbc0b2008d
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-18 18:03:20 +0900

    サブスクリプションの有効期限に関するロジックを削除し、最大値を計算して表示する機能を追加

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,23 +1,5 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
 
-if (daysUntilExpiration == 0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-    discountPercentage = 20;
-    Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
-}
-else if (daysUntilExpiration <= 5)
-{
-    discountPercentage = 10;
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
+Console.WriteLine(largerValue);

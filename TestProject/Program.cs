c90ede8ed227b873ee6002e63821e32e22897ecd
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-18 15:29:33 +0900

    サブスクリプションの有効期限に基づいてメッセージを表示するロジックを実装し、サイコロのロジックを削除

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,49 +1,23 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll : {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3))
+if (daysUntilExpiration == 0)
 {
-  if ((roll1 == roll2) && (roll2 == roll3))
-  {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
-  }
-  else
-  {
-    Console.WriteLine("You rolled a double! +2 bonus to total!");
-    total += 2;
-  }
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 
-if (total >= 16)
-{
-    Console.WriteLine("You win a new car!");
-}
-else if (total >= 10)
-{
-    Console.WriteLine("You win a new laptop!");
-}
-else if (total == 7)
-{
-    Console.WriteLine("You win a trip for two!");
-}
-else
-{
-    Console.WriteLine("You win a kitten!");
-}
-
-// string message = "The quick brown fox jumps over the lazy dog.";
-// bool result = message.Contains("dog");
-// Console.WriteLine(result);
-
-// if (message.Contains("fox"))
-// {
-//   Console.WriteLine("What does the fox say?");
-// }

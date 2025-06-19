23fcda741c32f1dcc4aea9144ee17a79e3430718
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-19 11:20:44 +0900

    文字列を反転し、'o'の出現回数をカウントして表示する機能を実装

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,9 +1,13 @@
-﻿string[] orderIds = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
-
-foreach (string orderId in orderIds)
-{
-  if (orderId.StartsWith("B"))
-  {
-    Console.WriteLine(orderId);
-  }
-}
+﻿string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");

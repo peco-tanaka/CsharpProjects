22dc32eda22d82bd8e6e0acc8a6973b6284fa852
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-21 19:17:34 +0900

    キャストを使用した暗黙的変換の例を整理

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,7 +1,16 @@
-﻿// 拡大変換により暗黙的な変換が実行される
+﻿/* // 拡大変換により暗黙的な変換が実行される
 int myInt = 3;
 Console.WriteLine($"int: {myInt}");
 
 decimal myDecimal = myInt;
 Console.WriteLine($"decimal: {myDecimal}");
 
+ */
+
+
+// キャストを使用して明示的に変換する例
+decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");

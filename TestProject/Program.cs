1dde52589081b41e457bc34263ff490b4d7970c8
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-21 19:59:15 +0900

    明示的なキャストを使用して decimal から float への型変換

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -8,9 +8,18 @@ Console.WriteLine($"decimal: {myDecimal}");
  */
 
 
-// キャストを使用して明示的に変換する例
+/* // キャストを使用して明示的に変換する例
 decimal myDecimal = 3.14m;
 Console.WriteLine($"decimal: {myDecimal}");
 
 int myInt = (int)myDecimal;
 Console.WriteLine($"int: {myInt}");
+ */
+
+
+// 明示的なキャストを使用して decimal から float への変換を行う例
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal; // 明示的なキャスト
+
+Console.WriteLine($"decimal: {myDecimal}");
+Console.WriteLine($"float: {myFloat}");

04f4012519e8cff8114124d1627e51304098559b
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-23 17:45:36 +0900

    無効な文字列を処理するための int.TryParse の使用を追加

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,20 +1,27 @@
-﻿/*
-// ToString()メソッドを使用して数値を文字列に変換
-int first = 5;
-int second = 7;
-string message = first.ToString() + second.ToString();
-Console.WriteLine(message);
-*/
+﻿/* string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
 
-/*
-// Parseメソッドを使用して文字列を数値に変換
-string first = "5";
-string second = "7";
-int sum = int.Parse(first) + int.Parse(second);
-Console.WriteLine(sum);
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
  */
 
-string value1 = "5";
-string value2 = "7";
-int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-Console.WriteLine(result)
+string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+   Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+   Console.WriteLine("Unable to report the measurement.");
+}
+
+if (result > 0)
+   Console.WriteLine($"Measurement (w/ offset): {50 + result}");

af74b96fcad38826648c4eb1625abaff9faab156
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-23 18:08:28 +0900

    無効な数値を処理し、合計を計算するための decimal.TryParse の使用を追加

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,27 +1,28 @@
-﻿/* string value = "102";
-int result = 0;
-if (int.TryParse(value, out result))
+﻿// 文字列の配列を用意。数値として解釈できるものとできないものが混在している
+string[] values = ["12.3", "45", "ABC", "11", "DEF"];
+
+// 数値に変換できなかった文字列を格納する変数
+string message = "";
+// 数値の合計を格納する変数
+decimal total = 0;
+
+// 配列の各要素を順に処理
+foreach (var value in values)
 {
-    Console.WriteLine($"Measurement: {result}");
-}
-else
-{
-    Console.WriteLine("Unable to report the measurement.");
+    decimal number;
+    // 文字列をdecimal型に変換できるか判定
+    if (decimal.TryParse(value, out number))
+    {
+        // 変換できた場合は合計に加算
+        total += number;
+    }
+    else
+    {
+        // 変換できなかった場合はmessageに追加
+        message += value;
+    }
 }
 
-Console.WriteLine($"Measurement (w/ offset): {50 + result}");
- */
-
-string value = "bad";
-int result = 0;
-if (int.TryParse(value, out result))
-{
-   Console.WriteLine($"Measurement: {result}");
-}
-else
-{
-   Console.WriteLine("Unable to report the measurement.");
-}
-
-if (result > 0)
-   Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+// 数値に変換できなかった文字列と合計値を出力
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");

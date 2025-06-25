7df815834d3e961fed7819c19458904aeacf9018
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-25 12:37:42 +0900

    顧客への新しい金融商品の紹介メッセージを追加し、比較情報を整形

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,11 +1,25 @@
-﻿string paymentId = "769C";
-string paymentName = "Mr. Stephen Ortega";
-string paymentAmount = "$5,000.00";
+﻿string customerName = "Ms. Barros";
 
-var formattedLine = paymentId.PadRight(6);
-formattedLine += paymentName.PadRight(24);
-formattedLine += paymentName.PadLeft(10);
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
 
-Console.WriteLine("1234567890123456789012345678901234567890");
-Console.WriteLine(formattedLine);
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
 
+// Your logic here
+
+Console.WriteLine($"Dear {customerName},\nAs a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be ¤{newProfit:N2}.\n");
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = string.Format("{0,-18}{1,8}   ${2,15:N2}\n{3,-18}{4,8}   ${5,15:N2}",
+    currentProduct, (currentReturn * 100).ToString("F2") + "%", currentProfit,
+    newProduct, (newReturn * 100).ToString("F2") + "%", newProfit);
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);

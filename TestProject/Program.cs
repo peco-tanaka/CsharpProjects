51a4b3efc6cf186447a1ab388acbf10305c61e04
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-25 12:20:19 +0900

    支払いIDと名前をフォーマットして表示

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,17 +1,11 @@
-﻿// 請求書番号を整数で定義
-int invoiceNumber = 1201;
-// 商品のシェア数（小数点以下4桁）
-decimal productShares = 25.4568m;
-// 小計（通貨型）
-decimal subtotal = 2750.00m;
-// 税率（小数）
-decimal taxPercentage = .15825m;
-// 合計金額（通貨型）
-decimal total = 3185.19m;
+﻿string paymentId = "769C";
+string paymentName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
 
-// 複合書式設定を使って各値をフォーマットして出力
-Console.WriteLine($"Invoice Number: {invoiceNumber}"); // 通常の整数表示
-Console.WriteLine($"   Shares: {productShares:N3} Product"); // 小数点以下3桁で3桁区切り
-Console.WriteLine($"     Sub Total: {subtotal:C}"); // 通貨書式
-Console.WriteLine($"           Tax: {taxPercentage:P2}"); // パーセント書式（小数点以下2桁）
-Console.WriteLine($"     Total Billed: {total:C}"); // 通貨書式
+var formattedLine = paymentId.PadRight(6);
+formattedLine += paymentName.PadRight(24);
+formattedLine += paymentName.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+

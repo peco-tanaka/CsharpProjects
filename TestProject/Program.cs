d023e830275311f9177e6c9e516d3f2f545baf93
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-25 10:25:29 +0900

    orderStreamをカンマ区切りで分割し、各アイテムの長さをチェックして表示

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,19 +1,15 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 
-// Step 1
-string[] message = pangram.Split(' ');
+string[] items = orderStream.Split(',');
 
-//Step 2
-string[] newMessage = new string[message.Length];
-
-// Step 3
-for (int i = 0; i < message.Length; i++)
+foreach (var item in items)
 {
-    char[] letters = message[i].ToCharArray();
-    Array.Reverse(letters);
-    newMessage[i] = new string(letters);
+    if (item.Length == 4)
+    {
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine(item + "\t- Error");
+    }
 }
-
-//Step 4
-string result = String.Join(" ", newMessage);
-Console.WriteLine(result);

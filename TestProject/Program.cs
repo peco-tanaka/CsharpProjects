7d66e58e25e3355904291e53a84d1b663939f264
peco-tanaka <ok.computeruk1929@gmail.com>
2025-06-25 11:22:26 +0900

    不要なコードを削除し、文字列フォーマットと出力処理を整理

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,15 +1,36 @@
-﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+﻿/* string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+ */
 
-string[] items = orderStream.Split(',');
+/* string first = "Hello";
+string second = "World";
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second); */
 
-foreach (var item in items)
-{
-    if (item.Length == 4)
-    {
-        Console.WriteLine(item);
-    }
-    else
-    {
-        Console.WriteLine(item + "\t- Error");
-    }
-}
+string first = "Hello";
+string second = "World";
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (SAVE {discount:C})");
+
+decimal measurement = 12345.678912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+Console.WriteLine($"Measurement: {measurement:N4} units");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+
+decimal price2 = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price2 - salePrice), price2);
+Console.WriteLine(yourDiscount);
+
+yourDiscount += $"A discount of {((price2 - salePrice) / price2):P2}!"; // inserted
+Console.WriteLine(yourDiscount);
